82624952268de6bbc2f8770e792f312998fc5331
itrytocodethings <imwaynebailey@gmail.com>
2022-05-30 13:33:21 -0400

    testing fix

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 from datetime import timedelta
 
 load_dotenv()
-print(os.getenv('DATABASE_URL'))
-
-SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL').replace('postgre', 'postgresql')
+URI = os.getenv('DATABASE_URL').replace('postgre', 'postgresql')
+print(URI)
+SQLALCHEMY_DATABASE_URI=URI
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 SECRET_KEY=os.getenv('SECRET_KEY')
 JWT_SECRET_KEY=os.getenv('SECRET_KEY')
