081a325f071e5884420d77dbc94b602db57c4995
itrytocodethings <imwaynebailey@gmail.com>
2022-05-30 13:37:44 -0400

    fix for heroku deploy

--- app/config.py
+++ app/config.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 from datetime import timedelta
 
 load_dotenv()
-URI = os.getenv('DATABASE_URL').replace('postgre', 'postgresql')
-print(URI)
-SQLALCHEMY_DATABASE_URI=URI
+# fix to deploy on heroku
+SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL').replace('postgres', 'postgresql')
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 SECRET_KEY=os.getenv('SECRET_KEY')
 JWT_SECRET_KEY=os.getenv('SECRET_KEY')
