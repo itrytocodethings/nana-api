05c235206bc07beda45a5f7f84f4345730154a15
itrytocodethings <imwaynebailey@gmail.com>
2022-05-30 13:09:10 -0400

    fixed config.py to load_dotenv to fix deploy on heroku

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
+load_dotenv()
+
 SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 SECRET_KEY=os.getenv('SECRET_KEY')
